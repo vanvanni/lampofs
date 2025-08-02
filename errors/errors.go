5744ce62dd74f7d193ff172d2ac828c59dec5377
Joey de Vries <uhmimjoey@gmail.com>
2025-08-03 01:17:21 +0200

    Remove unused error

--- errors/errors.go
+++ errors/errors.go
@@ -3,7 +3,6 @@ package errors
 import "errors"
 
 var (
-	ErrFileNotFound     = errors.New("file not found")
-	ErrFileExists       = errors.New("file already exists")
-	ErrPermissionDenied = errors.New("permission denied")
+	ErrFileNotFound = errors.New("file not found")
+	ErrFileExists   = errors.New("file already exists")
 )
